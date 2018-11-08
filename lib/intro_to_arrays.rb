69fe476a7563c19e236bc5f4c6c4bc4c51609f46
John Mark Redding <johnmarkredding@gmail.com>
2018-11-08 00:26:27 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,19 +4,19 @@ def instantiate_new_array
 end
 def array_with_two_elements
   arr = [2,3]
+  arr
 end
-def first_element
-      takes in an argument of an array and returns the first element in the array using its positive index (FAILED - 5)
+def first_element(arr)
+  arr[0]
 end
-def third_element
-      takes in an argument of an array and returns the third element in the array using its positive index (FAILED - 6)
+def third_element(arr)
+  arr[2]
 end
-def last_element
-      takes in an argument of an array and returns the last element in the array using its negative index (FAILED - 7)
+def last_element(arr)
+  arr[-1]
 end
-  using ruby array methods to return values from an array
-def first_element_with_array_methods
-      takes in an argument of an array and returns the first element in the array, without referencing the index number of that element (FAILED - 8)
+def first_element_with_array_methods(arr)
+  arr.first_element
 end
 def last_element_with_array_methods
       takes in an argument of an array and returns the last element in the array, wihtout referencing the index number (FAILED - 9)
